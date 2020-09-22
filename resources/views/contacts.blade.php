3403937b265817ecd3c596f8da59c377d151f1a8
Rafael <dev@itsparta.ru>
2020-09-22 15:05:20 +0300

    Добавил форму на сайт, изменил обработчик url адресов

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -6,4 +6,25 @@
 
 @section('content')
     <h1>Контакты</h1>
+
+    <form action="{{ route('contacts-form') }}" method="post">
+        @csrf
+        <div class="form-group">
+            <label for="name">Имя</label>
+            <input type="text" name="name" placeholder="Введите имя" id="name" class="form-control"/>
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="text" name="email" placeholder="Введите Email" id="email" class="form-control"/>
+        </div>
+        <div class="form-group">
+            <label for="subject">Тема сообщения</label>
+            <input type="text" name="subject" placeholder="Введите тему" id="subject" class="form-control"/>
+        </div>
+        <div class="form-group">
+            <label for="message">Сообщение</label>
+            <textarea name="message" id="message" placeholder="Введите сообщение" class="form-control"></textarea>
+        </div>
+        <button type="submit" class="btn btn-success">Отправить</button>
+    </form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('index');
+
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about');
+
 Route::get('/contacts', function () {
     return view('contacts');
-});
+})->name('contacts');
+
+Route::post('/contacts/submit', function () {
+    dd(Request::all());
+})->name('contacts-form');
