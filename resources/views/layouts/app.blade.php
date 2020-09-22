c8a8c79fab68692a900ca749ad6bba659e4c8155
Rafael <dev@itsparta.ru>
2020-09-22 11:27:53 +0300

    подключил стили и bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title>@yield('title-page')</title>
+    <link rel="stylesheet" src="/css/app.css">
+    <link rel="stylesheet" src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
     @yield('content')
